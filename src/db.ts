3aa1919a48dd6ad087ebb2bea757a5a23bcc8771
JoaoVictor <jmachadoferraz@gmail.com>
2025-09-17 02:08:31 -0300

    chore: remove .env do versionamento

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,8 @@
 import { MongoClient } from 'mongodb';
-import 'https://deno.land/std@0.224.0/dotenv/load.ts';
+if (Deno.env.get('DENO_DEPLOYMENT_ID') === undefined) {
+  // Somente em ambiente local
+  await import('https://deno.land/std@0.224.0/dotenv/load.ts');
+}
 
 const uri = Deno.env.get('MONGODB_URI');
 if (!uri) throw new Error('MONGODB_URI não definida no .env');
